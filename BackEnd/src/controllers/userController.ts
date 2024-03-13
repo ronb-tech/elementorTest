a45bd9b35c5d89e8166842b48bbb40ac419e3a12
ronb-tech <ronbersh@gmail.com>
2024-03-13 12:02:42 +0200

    small fix

--- BackEnd/src/controllers/userController.ts
+++ BackEnd/src/controllers/userController.ts
@@ -2,15 +2,11 @@ import { Request, Response } from "express";
 import { User } from "../models/types";
 import { getUsers } from "../utils/getMockData";
 
-// Mock users data, import it form other const file later
-const users: User[] = [
-  { id: 1, name: "John Doe", email: "john@example.com", username: "John" },
-  { id: 2, name: "Jane Doe", email: "jane@example.com", username: "Jane" },
-];
+let users: User[] = [];
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const users = await getUsers();
+    users = await getUsers();
     res.json(users);
   } catch (error) {
     //log the error

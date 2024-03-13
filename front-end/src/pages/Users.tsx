88dc37cf9046bc667a3fd00c3745272a493abcac
ronb-tech <ronbersh@gmail.com>
2024-03-13 16:25:30 +0200

    add call for get all users

--- front-end/src/pages/Users.tsx
+++ front-end/src/pages/Users.tsx
@@ -4,6 +4,23 @@ import { User } from "../utils/types";
 const UsersPage: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
+  const getUsers = async () => {
+    try {
+      const baseUrl = process.env.REACT_APP_BASE_URL;
+      const res = await fetch(`${baseUrl}/users`);
+      const data = await res.json();
+      return data;
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    getUsers().then((users) => {
+      if (users) {
+        setUsers(users);
+      }
+    });
+  }, []);
+
   return (
     <div className="page-users">
       <h1>Users Page</h1>

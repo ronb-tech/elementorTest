e6be33ff85ddf22d5f0ba86c43258317a79a243b
ronb-tech <ronbersh@gmail.com>
2024-03-13 15:38:30 +0200

    add not found page

---
+++ front-end/src/pages/Page404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page404: React.FC = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <span>The page you're looking for does not exist</span>
+    </div>
+  );
+};
+
+export default Page404;
--- front-end/src/route.tsx
+++ front-end/src/route.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Page404 from "./pages/Page404";
 import Users from "./pages/Users";
 import Albums from "./pages/Albums";
 import Photos from "./pages/Photos";
@@ -8,6 +9,7 @@ export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate replace to="/users" />} />
+      <Route path="*" element={<Page404 />} /> {}
       <Route path="/users" element={<Users />} />
       <Route path="/users/:userId/albums" element={<Albums />} />
       <Route path="/albums/:albumId/photos" element={<Photos />} />

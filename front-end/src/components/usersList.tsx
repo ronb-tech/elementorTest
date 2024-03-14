8571a55e363dbfc6e8520294de95c0267fcd9219
ronb-tech <ronbersh@gmail.com>
2024-03-14 16:55:53 +0200

    add user avatar

--- front-end/src/components/usersList.tsx
+++ front-end/src/components/usersList.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { User } from "../utils/types";
+import { onImgError } from "../utils/helper";
 
 interface UsersListProps {
   users: User[];
@@ -18,6 +20,13 @@ const UsersList: React.FC<UsersListProps> = ({ users, onUserClick }) => {
           key={user._id}
           onClick={() => onUserClick(user._id)}
         >
+          <CardMedia
+            component="img"
+            className="card-img-avatar"
+            image={user.avatarUrl}
+            title={user.username}
+            onError={onImgError}
+          />
           <CardContent>
             <Typography variant="h5">{user.name}</Typography>
             <Typography variant="body2">Email: {user.email}</Typography>

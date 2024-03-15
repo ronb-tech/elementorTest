132be9cd9122181bc1c1546c25f550ef519c9ad5
ronb-tech <ronbersh@gmail.com>
2024-03-15 02:13:33 +0200

    small fix

--- BackEnd/src/controllers/albumController.ts
+++ BackEnd/src/controllers/albumController.ts
@@ -2,6 +2,22 @@ import { Request, Response } from "express";
 import { Album } from "../models/types";
 import { getAlbumsByUserIdData } from "../services/albumService";
 
+export const getAllAlbumsByUserId = async (req: Request, res: Response) => {
+  const user_id = parseInt(req.params.user_id);
+  let albums: Album[] = [];
+
+  try {
+    if (user_id <= 0 || isNaN(user_id)) {
+      return res.status(400).json({ message: "Invalid 'user_id' provided." });
+    }
+    albums = await getAlbumsByUserIdData(user_id);
+    res.json(albums);
+  } catch (error) {
+    console.error("Failed to fetch albums:", error);
+    res.status(500).json({ message: "Failed to fetch albums" });
+  }
+};
+
 export const getAlbumsByUserId = async (req: Request, res: Response) => {
   const user_id = parseInt(req.params.user_id);
   let albums: Album[] = [];
--- BackEnd/src/routes/albumRoutes.ts
+++ BackEnd/src/routes/albumRoutes.ts
@@ -1,7 +1,10 @@
 import express from "express";
-import { getAlbumsByUserId } from "../controllers/albumController";
+import {
+  getAllAlbumsByUserId,
+  getAlbumsByUserId,
+} from "../controllers/albumController";
 
 const albumRoutes = express.Router();
-albumRoutes.get("/:user_id", getAlbumsByUserId);
+albumRoutes.get("/:user_id", getAllAlbumsByUserId);
 
 export default albumRoutes;

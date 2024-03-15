4621aa38c7d389ae370f97209a9b74f0858f54f7
ronb-tech <ronbersh@gmail.com>
2024-03-15 11:16:36 +0200

    fix route

--- BackEnd/src/routes/photoRoutes.ts
+++ BackEnd/src/routes/photoRoutes.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import { getPhotos } from "../controllers/photoController";
+import {
+  getPhotos,
+  deletePhotoById,
+  addPhoto,
+} from "../controllers/photoController";
 
 const photosRoutes = express.Router();
 photosRoutes.get("/:album_id", getPhotos);
+photosRoutes.put("/update", deletePhotoById);
+photosRoutes.post("/create", addPhoto);
 
 export default photosRoutes;

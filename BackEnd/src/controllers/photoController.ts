6044497815e3d7d3fb9256fd8191a073545f7fff
ronb-tech <ronbersh@gmail.com>
2024-03-15 11:13:19 +0200

    fix addPhoto

--- BackEnd/src/controllers/photoController.ts
+++ BackEnd/src/controllers/photoController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { Photo } from "../models/types";
 import { getAllPhotos } from "../utils/getMockData";
+import {
+  deleteItem,
+  updateItem,
+  addItem,
+  fetchData,
+  mapHandles,
+} from "../models/dataHandler";
 
 let photos: Photo[] = [];
 
@@ -11,7 +18,9 @@ export const getPhotos = async (req: Request, res: Response) => {
     if (album_id <= 0 || isNaN(album_id)) {
       return res.status(400).json({ message: "Invalid 'album_id' provided." });
     }
-    photos = await getAllPhotos();
+    photos = await fetchData<Photo>({
+      ...mapHandles.photos.allImages,
+    });
     const photosByAlbumId = photos.filter(
       (photo) => photo.albumId === album_id
     );
@@ -19,5 +28,31 @@ export const getPhotos = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Failed to fetch photos:", error);
     res.status(500).json({ message: "Failed to fetch photos" });
+  }
+};
+
+export const addPhoto = async (req: Request, res: Response) => {
+  const photoData = req.body;
+  if (!photoData) {
+    return res
+      .status(400)
+      .send({ message: "Photo data is missing", ok: false });
+  }
+
+  try {
+    const response = await addItem({
+      ...mapHandles.photos.allImages,
+      item: photoData,
+    });
+    if (response) {
+      res
+        .status(201)
+        .send({ message: "Photo added successfully", response, ok: true });
+    } else {
+      throw new Error("Failed to add photo");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Server error", ok: false });
   }
 };
